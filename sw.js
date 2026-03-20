b7660c9650de7295ed393e1f528b8b2dee3d2b71
riccardolmomanca-dot <riccardolmo.manca@gmail.com>
2026-03-20 21:28:15 +0100

    Upgrade service worker to v2 with cache management
    Updated cache version and added activate event listener for cache cleanup.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gymtrack-v1';
+const CACHE_NAME = 'gymtrack-v2';
 const ASSETS = [
   'index.html',
   'manifest.json'
@@ -7,6 +7,14 @@ const ASSETS = [
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
 
